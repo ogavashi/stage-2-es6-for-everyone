922825a13ca7dfcd539983b41f0565430046312a
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2022-06-08 20:27:31 +0300

    Switched to github API without token

--- src/javascript/helpers/apiHelper.js
+++ src/javascript/helpers/apiHelper.js
@@ -9,7 +9,7 @@ const SECURITY_HEADERS = {
      * To test the application against the real dataset set useMockAPI=false.
      * But to test the application you don't need to extend the GitHub REST API rate limit to 5000 requests with the token
      */
-    authorization: 'token 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'
+    //authorization: 'token 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'
   }
 };
 
